429401a471a334eb3502ab4865a615d23852563b
Alice Weng <alicewengs@gmail.com>
2024-06-02 18:46:11 -0700

    added a fetch request for all champion names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  let [champions, setChampions] = useState([]);
+
+  useEffect(() => {
+    fetchChampions();
+  }, []);
+
+  const fetchChampions = async () => {
+    const response = await fetch("https://ddragon.leagueoflegends.com/cdn/14.11.1/data/en_US/champion.json");
+    const championsData = await response.json();
+    setChampions(Object.keys(championsData.data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
