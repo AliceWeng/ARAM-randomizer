a726d5749c70cf3303d1e8f9afb7e5a676431bf1
Alice Weng <alicewengs@gmail.com>
2024-06-05 10:07:27 -0700

    added a function to randomly select 10 unique champions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   let [champions, setChampions] = useState([]);
+  let [blue, setBlue] = useState([]);
+  let [red, setRed] = useState([]);
 
   useEffect(() => {
     fetchChampions();
@@ -14,13 +16,49 @@ function App() {
     setChampions(Object.keys(championsData.data));
   };
 
+  const createTeams = () => {
+    let numbers = [];
+    let blue = [];
+    let red = [];
+    for(let i = 0; i < 10; i++) {
+      let number = Math.floor(Math.random() * champions.length);
+      if(numbers.includes(number)) {
+        i--;
+      } else {
+        numbers.push(number);
+        if(numbers.length <= 5) {
+          blue.push(number);
+        } else red.push(number);
+      }
+    }
+    setBlue(blue);
+    setRed(red);
+  }
+
+  let mapTeam = team => team.map(index => {
+      return (
+        <div key={index}>
+          <img src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${champions[index]}.png`} alt={champions[index]}/>
+          <p>{champions[index]}</p>
+        </div>
+      )
+    });
+
   return (
     <div>
       <header>
 
       </header>
       <main>
-
+        <div className="versus">
+          <div>
+            {mapTeam(blue)}
+          </div>
+          <button onClick={() => createTeams()}>Accept!</button>
+          <div>
+            {mapTeam(red)}
+          </div>
+        </div>
       </main>
     </div>
   );
