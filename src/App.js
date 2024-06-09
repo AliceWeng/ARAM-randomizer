93241bc1920450dee95e22a554b36c06f712814f
Alice Weng <alicewengs@gmail.com>
2024-06-08 23:00:12 -0700

    added blue and red background, added option to turn off visibility of either team

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   let [ids, setIds] = useState([]);
   let [names, setNames] = useState([]);
-  let [blue, setBlue] = useState([]);
-  let [red, setRed] = useState([]);
+  let [blue, setBlue] = useState(["a", "b", "c", "d", "e"]);
+  let [red, setRed] = useState(["a", "b", "c", "d", "e"]);
+  let [hideBlue, setHideBlue] = useState(true);
+  let [hideRed, setHideRed] = useState(true);
 
   useEffect(() => {
     fetchChampions();
@@ -38,17 +40,37 @@ function App() {
     setRed(red);
   }
 
-  let mapTeam = team => team.map((number, index) => {
-      return (
-        <div key={index} className="summoner">
-          <img src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${ids[number]}.png`} alt={names[number]}/>
-          <div>
-            <p>{names[number]}</p>
-            <input placeholder={`Summoner ${index + 1}`} maxLength="16"/>
-          </div>
+  let mapBlue = blue.map((number, index) => {
+    return (
+      <div key={index} className="summoner">
+        { typeof blue[0] === "string" || hideBlue
+        ? <img src="https://ddragon.leagueoflegends.com/cdn/14.11.1/img/profileicon/29.png" alt="default profile icon"/>
+        : <img src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${ids[number]}.png`} alt={names[number]}/> }
+        <div>
+          { typeof blue[0] === "string" || hideBlue
+          ? null
+          : <p>{names[number]}</p> }
+          <input placeholder={`Summoner ${index + 1}`} maxLength="16" spellCheck="false"/>
         </div>
-      )
-    });
+      </div>
+    )
+  });
+
+  let mapRed = red.map((number, index) => {
+    return (
+      <div key={index} className="summoner">
+        <div>
+          { typeof red[0] === "string" || hideRed
+          ? null
+          : <p>{names[number]}</p> }
+          <input placeholder={`Summoner ${index + 1}`} maxLength="16" spellCheck="false"/>
+        </div>
+        { typeof red[0] === "string" || hideRed
+        ? <img src="https://ddragon.leagueoflegends.com/cdn/14.11.1/img/profileicon/29.png" alt="default profile icon"/>
+        : <img src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${ids[number]}.png`} alt={names[number]}/> }
+      </div>
+    )
+  });
 
   return (
     <div>
@@ -56,14 +78,21 @@ function App() {
         <h1>ARAM Randomizer</h1>
       </header>
       <main>
-        <div className="versus">
-          <div>
-            {mapTeam(blue)}
+        <div>
+          {mapBlue}
+        </div>
+          <div className="middle">
+            <button onClick={() => createTeams()}>Accept!</button>
+            <h2>Toggle Visibility</h2>
+            <div>
+              <label>Blue Team</label>
+              <input type="checkbox" onClick={() => setHideBlue(!hideBlue)} checked={hideBlue}/>
+              <label>Red Team</label>
+              <input type="checkbox" onClick={() => setHideRed(!hideRed)} checked={hideRed}/>
+            </div>
           </div>
-          <button onClick={() => createTeams()}>Accept!</button>
-          <div>
-            {mapTeam(red)}
-          </div>
+        <div>
+          {mapRed}
         </div>
       </main>
     </div>
